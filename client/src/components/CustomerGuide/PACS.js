c2bf009c77a93c8dd9591b15c371358a2b0fdf2f
Caomy0 <mynho1132002@gmail.com>
2024-10-25 23:44:57 +0700

    update

--- client/src/components/CustomerGuide/PACS.js
+++ client/src/components/CustomerGuide/PACS.js
@@ -99,7 +99,6 @@ const renderInfoSection = () => (
           width: "1200px",
         }}
       >
-
         Từ ngày 07/08/2023, Phòng Khám Bệnh viện Đại học Y Dược 1 triển khai
         hình thức trả kết quả chẩn đoán hình ảnh thông qua hệ thống PACS thay
         thế in phim đối với các dịch vụ kỹ thuật: Chụp cộng hưởng từ (MRI), Chụp
@@ -108,25 +107,13 @@ const renderInfoSection = () => (
     </Col>
   </Row>
 );
+
 const renderDefinitionSection = () => (
   <Col style={{}}>
     <Row>
       <h5 className="text-xl font-bold text-blue-900" style={{}}>
         1. Pacs là gì?
       </h5>
-
-        Từ ngày 07/08/2023, Phòng Khám Bệnh viện Đại học Y Dược 1 triển khai hình thức trả kết quả chẩn đoán hình ảnh thông qua hệ thống PACS thay thế in phim đối với các dịch vụ kỹ thuật: Chụp cộng hưởng từ (MRI), Chụp cắt lớp vi tính (CT-Scan), X-Quang kỹ thuật số cho khách hàng.
-      </h5>
-    </Col>
-  </Row>
-
-);
-
-const renderDefinitionSection = () => (
-  <Col style={{}}>
-    <Row>
-      <h5 className="text-xl font-bold text-blue-900" style={{}}>1. Pacs là gì?</h5>
-
     </Row>
     <Row>
       <p
@@ -136,7 +123,6 @@ const renderDefinitionSection = () => (
           fontSize: "1.1rem",
         }}
       >
-
         PACS (Picture Archiving and Communication Systems) là hệ thống lưu trữ
         và truyền tải hình ảnh y tế mang lại nhiều lợi ích cho Khách hàng và Bác
         sĩ như:
@@ -159,21 +145,29 @@ const renderDefinitionSection = () => (
         <br />- Bản giấy kết quả.
         <br />- Email của người bệnh/ người đại diện Khách hàng.
         <br />- Tin nhắn SMS qua số điện thoại của Khách hàng/ Người đại diện
-        Khách hàng.
-
-        PACS (Picture Archiving and Communication Systems) là hệ thống lưu trữ và truyền tải hình ảnh y tế mang lại nhiều lợi ích cho Khách hàng và Bác sĩ như:
-        <br />- Khách hàng không phải chờ đợi in, trả phim, rút ngắn thời gian khám, chữa bệnh;
-        <br />- Truy cập được toàn bộ dữ liệu, hình ảnh khách hàng đã chụp tại Phòng Khám một cách nhanh chóng, chính xác;
-        <br />- Thuận tiện tra cứu, chia sẻ dữ liệu, hình ảnh mọi lúc mọi nơi, trong và ngoài nước;
-        <br />- Hiệu quả cao, chính xác, nhanh chóng trong việc hội chẩn từ xa, so sánh các lần chụp, đối chiếu kết quả, giúp nâng cao chất lượng khám, chữa bệnh;
-        <br />- Giúp quản lý chặt chẽ, lâu dài kết quả chẩn đoán hình ảnh của Khách hàng;
+        Khách hàng. PACS (Picture Archiving and Communication Systems) là hệ
+        thống lưu trữ và truyền tải hình ảnh y tế mang lại nhiều lợi ích cho
+        Khách hàng và Bác sĩ như:
+        <br />- Khách hàng không phải chờ đợi in, trả phim, rút ngắn thời gian
+        khám, chữa bệnh;
+        <br />- Truy cập được toàn bộ dữ liệu, hình ảnh khách hàng đã chụp tại
+        Phòng Khám một cách nhanh chóng, chính xác;
+        <br />- Thuận tiện tra cứu, chia sẻ dữ liệu, hình ảnh mọi lúc mọi nơi,
+        trong và ngoài nước;
+        <br />- Hiệu quả cao, chính xác, nhanh chóng trong việc hội chẩn từ xa,
+        so sánh các lần chụp, đối chiếu kết quả, giúp nâng cao chất lượng khám,
+        chữa bệnh;
+        <br />- Giúp quản lý chặt chẽ, lâu dài kết quả chẩn đoán hình ảnh của
+        Khách hàng;
         <br />- Giảm tác hại cho môi trường do việc in phim gây ra.
-        <br /><br />
-        Sau khi có kết quả chụp, Khách hàng được cung cấp đường link, tên tài khoản và mật khẩu để truy cập vào hệ thống PACS qua các hình thức:
+        <br />
+        <br />
+        Sau khi có kết quả chụp, Khách hàng được cung cấp đường link, tên tài
+        khoản và mật khẩu để truy cập vào hệ thống PACS qua các hình thức:
         <br />- Bản giấy kết quả.
         <br />- Email của người bệnh/ người đại diện Khách hàng.
-        <br />- Tin nhắn SMS qua số điện thoại của Khách hàng/ Người đại diện Khách hàng.
-
+        <br />- Tin nhắn SMS qua số điện thoại của Khách hàng/ Người đại diện
+        Khách hàng.
       </p>
     </Row>
   </Col>
@@ -185,13 +179,6 @@ const renderAccessGuideSection = () => (
       padding: "30px 0px",
     }}
   >
-
-
-const renderAccessGuideSection = () => (
-  <Col style={{
-    padding: "30px 0px",
-  }}>
-
     <Row>
       <h5 className="text-xl font-bold text-blue-900">2. Hướng dẫn truy cập</h5>
     </Row>
@@ -203,12 +190,9 @@ const renderAccessGuideSection = () => (
           fontSize: "1.1rem",
         }}
       >
-
         Quý Khách hàng vui lòng tham khảo hướng dẫn đăng nhập, sử dụng và quản
-        lý tài khoản truy cập:
-
-        Quý Khách hàng vui lòng tham khảo hướng dẫn đăng nhập, sử dụng và quản lý tài khoản truy cập:
-
+        lý tài khoản truy cập: Quý Khách hàng vui lòng tham khảo hướng dẫn đăng
+        nhập, sử dụng và quản lý tài khoản truy cập:
       </p>
       <img
         src="https://umcclinic.com.vn/Data/Sites/1/media/h%C6%B0%E1%BB%9Bng-d%E1%BA%ABn-kh/h%C6%B0%E1%BB%9Bng-d%E1%BA%ABn-truy-c%E1%BA%ADp-h%E1%BB%87-th%E1%BB%91ng-l%C6%B0u-tr%E1%BB%AF-v%C3%A0-qu%E1%BA%A3n-l%C3%BD-h%C3%ACnh-%E1%BA%A3nh-(pacs)/pacs.jpg"
