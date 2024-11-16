bfe32b9cd9676fbe7b7973a7927f349ab4fa593c
Khai-Git <kelvinnon28092001@gmail.com>
2024-11-16 17:01:54 +0700

    Upload code from commit "update chat"

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,7 @@ import { Layout } from "antd";
 import HeaderComponent from "../components/Header/Header";
 import SectionComponent from "../components/Section/Section";
 import FooterComponent from "../components/component/Footer";
+import { useNavigate, Link } from "react-router-dom";
 
 const { Content } = Layout;
 
@@ -21,22 +22,17 @@ const Home = () => {
   // }, []);
 
   return (
-    <Layout className="layout">
-      <HeaderComponent />
-      <Content style={{ padding: "0 50px" }}>
-        <div className="site-layout-content" style={{ padding: "50px 0" }}>
-          <SectionComponent />
-        </div>
-      </Content>
-      <FooterComponent />
-      Thêm thẻ df-messenger ở dưới cùng để Dialogflow xuất hiện
-      <df-messenger
-        intent="WELCOME"
-        chat-title="Chat-Hospital123"
-        agent-id="fc9b1715-8b10-4943-99db-b7d6d0f87717"
-        language-code="vi"
-      ></df-messenger>
-    </Layout>
+    <div>
+      <Layout className="layout">
+        <HeaderComponent />
+        <Content style={{ padding: "0 50px" }}>
+          <div className="site-layout-content" style={{ padding: "50px 0" }}>
+            <SectionComponent />
+          </div>
+        </Content>
+        <FooterComponent />
+      </Layout>
+    </div>
   );
 };
 
